4dd0a36ad29e9feefe7f9d8b1b06ed5932936fda
neesetifa <36785948+neesetifa@users.noreply.github.com>
2019-06-13 23:23:48 -0700

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -16,9 +16,6 @@ class Agent:
         self.target_network=keras.models.clone_model(network_model)
         self.target_network.set_weights(network_model.get_weights())
         
-        #for cnn
-        #self.target_q_values_func=K.function([self.target_network.layers[0].input], [self.target_network.layers[5].output])
-        #for nn
         self.target_q_values_func=K.function([self.target_network.layers[0].input], [self.target_network.layers[3].output])
 
         
@@ -43,13 +40,10 @@ class Agent:
     def update_target_network(self):
         self.target_network.set_weights(self.q_network.get_weights())
     
-    
-    #用于test, 完全信赖model
     def greedy_policy(self,q_values):
         return np.argmax(q_values)
     
     
-    # 用于train,即并不完全信任model,让agent有机会随机选择动作
     def greedy_epsilon_policy(self,q_values):
         epsilon=0.05
         rnd = random.random()
@@ -57,7 +51,7 @@ class Agent:
             return random.randint(0, 4)
         
         return np.argmax(q_values)
-    # 用于train,让agent随机选择的几率慢慢下降,从0.999下降到0.05,即我们对model选择的信任度随着训练时长逐渐上升
+
     def linear_greedy_epsilon_policy(self,q_values):
         if self.num_step>949000:
             epsilon=0.05
@@ -113,74 +107,52 @@ class Agent:
         # i.e.  action 3 -->  [0,0,0,1,0]
         actions=self.transform_actions(actions)
         
-        #用旧的网络w-,选择值最高的q_value
         q_values=self.target_q_values_func([new_states])[0]
         max_q_values = np.max(q_values, axis=1)
         
-        #把是terminal的q_value置为0
+
         max_q_values[is_terminals] = 0
-        # gamma衰减因子  设置 0.99
         targets = rewards + 0.99* max_q_values
         targets = np.expand_dims(targets, axis=1)
-        #在一个batch的数据上进行一次参数更新
-        #因为我们是以一个batch,aka 32个经验数组去update的
         self.q_network.train_on_batch([states, actions], targets)
         
         if self.num_step%19000==0:
             self.update_times+=1
             self.update_target_network()
-            #新的=====================================================================================
             if self.update_times>0 and self.update_times%30==0:
                 self.q_network.save_weights('./model_weights_round_%d.h5' % (self.update_times))
-            #=========================================================================================
+
 
     def update_ddqn(self):
         states, actions, rewards, new_states, is_terminals=self.memoryD.get_sample()
         # i.e.  action 3 -->  [0,0,0,1,0]
         actions=self.transform_actions(actions)
         
-        #用新的权重w选择动作.  即选择q_value最大值对应的动作
+
         q_values=self.q_values_func([new_states])[0]
         max_actions = np.argmax(q_values, axis=1)
         
-        #用获得的动作组合成index方便下面使用
+
         tmp=np.arange(0,len(max_actions))
         index=np.stack((tmp,max_actions),axis=0)
         
-        #用旧的权重w-获得q_values
+
         target_q_values=self.target_q_values_func([new_states])[0]
-        #不用最大值作为max_q_value,而是用刚才动作的index作为max_q_value
         max_q_values=target_q_values[list(index)]
         
         
-        #把是terminal的q_value置为0
+
         max_q_values[is_terminals] = 0
-        # gamma衰减因子  设置 0.99
         targets = rewards + 0.99* max_q_values
         targets = np.expand_dims(targets, axis=1)
-        #在一个batch的数据上进行一次参数更新
-        #因为我们是以一个batch,aka 32个经验数组去update的
         self.q_network.train_on_batch([states, actions], targets)
         
         if self.num_step%19000==0:
             self.update_times+=1
             self.update_target_network()
-            #新的=====================================================================================
             if self.update_times>0 and self.update_times%30==0:
                 self.q_network.save_weights('./model_weights_round_%d.h5' % (self.update_times))
-            #=========================================================================================
-        
-'''
-举例,有数组a
-a=np.array([[0,9,1],[2,3,4],[1,3,2],[9,7,1]])
-我知道b=np.array([1,1,0,2]),是每一行我想要的index
-即我想要获得数组[9,3,1,1]
-构造数组
-c=np.arange(0,4)
-d=np.stack((c,b),axis=0)
-d为 [[0,1,2,3],[1,1,0,2]]
 
-a[list(d)]即为一维的[9,3,1,1]
-'''
+        
         
                 
